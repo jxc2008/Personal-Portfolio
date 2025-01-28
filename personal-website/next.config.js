aa8d24eec2e8f0167349dca207ddeacfe7b25b7e
jxc2008 <jxc2008@nyu.edu>
2025-01-27 23:09:45 -0500

    next config

---
+++ personal-website/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true, // Enforces best practices in React
+    swcMinify: true, // Enables faster builds with the SWC compiler
+  };
+  
+  module.exports = nextConfig;
+  
